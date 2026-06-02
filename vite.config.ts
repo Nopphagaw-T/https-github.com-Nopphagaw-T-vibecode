76be9563f842516f4c796cffaffe11f162346489
nopphagaw <nopphagaw.t@fitm.kmutnb.ac.th>
2026-06-02 15:30:18 +0700

    chore: initialize local database and update API proxy target port

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(() => {
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
       proxy: {
         '/api': {
-          target: 'http://127.0.0.1:5001',
+          target: 'http://localhost:5000',
           changeOrigin: true,
         },
       },
